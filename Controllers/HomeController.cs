7e4398924c338de203f09c754e5ac72b20238c82
bamcavoy <120762820+bamcavoy@users.noreply.github.com>
2024-04-09 10:46:58 -0600

    pagination

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using BrickVault.Models;
+using BrickVault.Models.ViewModels;
 
 namespace BrickVault.Controllers;
 
@@ -17,9 +18,33 @@ public class HomeController : Controller
         return View();
     }
     
-    public IActionResult Products()
+    public IActionResult Products(int pageNum)
     {
-        return View();
+        int pageSize = 10;
+        
+        var model = new ProductListViewModel
+        {
+            // Products = _repo.Products
+            //     .OrderBy(x => x.Title)
+            //     .Skip((pageNum - 1) * pageSize)
+            //     .Take(pageSize),
+        
+            // PaginationInfo = new PaginationInfo
+            // {
+            //     CurrentPage = pageNum,
+            //     ItemsPerPage = pageSize,
+            //     TotalItems = _repo.Products.Count()
+            // }
+            
+            PaginationInfo = new PaginationInfo
+            {
+                CurrentPage = 1,
+                ItemsPerPage = 5,
+                TotalItems = 37
+            }
+        };
+
+        return View(model);
     }
     
     public IActionResult AboutUs()
