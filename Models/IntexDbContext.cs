0c6ce9634d97797b53cbbf0e5a8165538ad8d23f
bamcavoy <120762820+bamcavoy@users.noreply.github.com>
2024-04-10 15:43:56 -0600

    connection string hiding again

--- Models/IntexDbContext.cs
+++ Models/IntexDbContext.cs
@@ -27,9 +27,6 @@ public partial class IntexDbContext : DbContext
 
     public virtual DbSet<ProductCategory> ProductCategories { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer("Server=tcp:myfreesqldbservermcavoy.database.windows.net,1433;Initial Catalog=McAvoyDB;Persist Security Info=False;User ID=blakemcavoy;Password=inteX2024;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Category>(entity =>
