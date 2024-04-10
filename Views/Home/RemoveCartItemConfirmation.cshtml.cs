44fbc0e9db80331648801816a5311a9681f2110e
elijahaken <elijahaken73@gmail.com>
2024-04-09 20:04:01 -0600

    ...

---
+++ Views/Home/RemoveCartItemConfirmation.cshtml.cs
@@ -0,0 +1,6 @@
+namespace BrickVault.Views.Home;
+
+public class RemoveCartItemConfirmation_cshtml
+{
+    //Are you sure you want to remove the item.
+}
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,35 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
+window.onload = function() {
+    if (!getCookie("cookiesAccepted")) {
+        document.getElementById("cookieConsentPopup").style.display = "block";
+    }
+};
+
+function acceptCookies() {
+    setCookie("cookiesAccepted", "true", 365);
+    document.getElementById("cookieConsentPopup").style.display = "none";
+}
+
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
 
 // Write your JavaScript code.
